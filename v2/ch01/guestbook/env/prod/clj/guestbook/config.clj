a08d3a2dd43016833b2eea84cc719540c5233db1
Tim Hawes <tim.hawes@inin.com>
2016-04-08 15:42:09 -0400

    Restarting web project

--- v2/ch01/guestbook/env/prod/clj/guestbook/config.clj
+++
@@ -1,8 +0,0 @@
-(ns guestbook.config
-  (:require [clojure.tools.logging :as log]))
-
-(def defaults
-  {:init
-   (fn []
-     (log/info "\n-=[guestbook started successfully]=-"))
-   :middleware identity})
