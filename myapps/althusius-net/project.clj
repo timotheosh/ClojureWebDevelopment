9ef2b7ad844019f697f92bd62ad41fd1cb69edfd
Tim Hawes <tim.hawes@inin.com>
2016-06-23 20:35:40 -0400

    Added stasis project

---
+++ myapps/althusius-net/project.clj
@@ -0,0 +1,12 @@
+(defproject althusius-net "0.1.0-SNAPSHOT"
+  :description "Althusius.net Website"
+  :url "http://althusius.net"
+  :license {:name "Apache 2.0"
+            :url "https://opensource.org/licenses/Apache-2.0"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [stasis "1.0.0"]
+                 [ring "1.2.1"]
+                 [hiccup "1.0.5"]
+                 [me.raynes/cegdown "0.1.1"]]
+  :ring {:handler althusius-net.web/app}
+  :profiles {:dev {:plugins [[lein-ring "0.8.10"]]}})
---
+++ myapps/althusius-net/resources/md/my-first-post.md
@@ -0,0 +1,3 @@
+# My first post
+
+It's pretty short for now
